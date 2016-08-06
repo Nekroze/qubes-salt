e80e21ebbee95b3d19ae22c9f4f5784406d5136d
Taylor "Nekroze" Lawson <tlawson@nekroze.com>
2016-08-06 17:09:19 +1000

    Expanded update-salt script to ensure it is run as root and the /srv/salt repo is setup

--- update-salt.sh
+++ update-salt.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+  echo "Must be run as root!"
+  exit 1
+fi
+
 cd /srv/salt
-git pull
+
+if [ ! -d /srv/salt/.git ]; then
+  git init
+  git remote add origin /home/nekroze/salt
+  git fetch
+  git checkout -t origin/master
+else
+  git pull
+fi
